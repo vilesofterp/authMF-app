8261f1c0f615a1034d9bd9acddc11b2b6f86fe54
roger-maia-us <roger@vilesoft.us>
2023-12-19 16:54:01 -0500

    12-19-2023

---
+++ Api/Dtos/AuthMfLogDto.cs
@@ -0,0 +1,37 @@
+﻿
+using ZionApi;
+
+namespace Api.Dtos
+{
+    public class AuthMfLogDto : ZionValidation
+    {
+        public long id { get; set; }
+        public long id_partner { get; set; }
+        public long id_user { get; set; }
+        public DateTime date { get; set; }
+        public int success { get; set; }
+        public AuthMfLogDto()
+        {
+            MapperValidation();
+        }
+
+        void MapperValidation()
+        {
+            Validation["Id_user"] = new Dictionary<string, object>
+            {
+                {"MIN_VAL", 1},
+            };
+
+            Validation["Id_partner"] = new Dictionary<string, object>
+            {
+                {"MIN_VAL", 1},
+            };
+
+            Validation["success"] = new Dictionary<string, object>
+            {
+                {"LOGICAL", 1},
+            };
+        }
+    }
+}
+
