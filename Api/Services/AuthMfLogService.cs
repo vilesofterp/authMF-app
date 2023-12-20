39c17f03f56342a3024e00f14d16517858b76d53
roger-maia-us <roger@vilesoft.us>
2023-12-20 18:08:58 -0500

    12-20-23 18:00

--- Api/Services/AuthMfLogService.cs
+++ Api/Services/AuthMfLogService.cs
@@ -1,33 +1,26 @@
 ﻿using ZionApi;
 using ZionOrm;
 using ModelVAS;
-using Newtonsoft.Json.Linq;
 using Api.Dtos;
+using System.Data;
 
-namespace VasLog.Services
+namespace Api.Services
 {
     public class AuthMfLogService : ZionCrud
     {
-        public AuthMfLogService(string token_partner, long id_user, int success): base(request: "")
+        public AuthMfLogService(string token_partner, long id_user, int success) : base(request: new CustomRequestForm())
         {
             serviceName = "AuthMfLog";
             tableName = "auth_mf_log";
             dataModel = new AuthMfLogModel();
             dto = new AuthMfLogDto();
-            json = LoadPartner(token_partner);
-            dto.id_partner = GetJsonRecord(1, "id");
+            DataTable tb = LoadPartner(token_partner);
+            dto.id_partner = (long)tb.Rows[0]["ID"];
             dto.id_user = id_user;      
             dto.success = success;
-
-            /*
-             * Preste atenção em alterações neste serviço: 
-             * Pois, gravar a tabela auth_mf_log, ela dispara uma cadeia de triggers no banco de dados para gerar estatisticas de requisição por partner
-             *
-             **/
-            AuthMfLogModel AuthMfLogModel = new();
-            dynamic model = new ZionModel(AuthMfLogModel, "auth_mf_log", "2");
-            json = ZionDto.MapperModel(dto, ref model.RecordNew);
-            json = model.Save("AuthMfLog");
+            ZionModel model = new ZionModel(dataModel, "auth_mf_log", "2");
+            json = ZionDto.MapperModel(in dto, ref model.RecordNew);
+            json = model.Save("AuthMfLogService");
         }
     }
 }
