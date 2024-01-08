4a9dd99d0d1d7729e311c7e92321d80660d032d3
roger-maia-us <roger@vilesoft.us>
2024-01-08 14:58:11 -0500

    1 8 24

--- Api/Services/ValidadeCodeService.cs
+++ Api/Services/ValidadeCodeService.cs
@@ -3,6 +3,9 @@ using ZionOrm;
 using ModelVAS;
 using ZionHelper;
 using VasLog.Services;
+using System.Data;
+using ZionApi;
+using Newtonsoft.Json.Linq;
 
 namespace Api.Services
 {
@@ -18,19 +21,41 @@ namespace Api.Services
 
         public dynamic ValidateCode()
         {
-            // Flow Crud
-            ZionModel model = new ZionModel(dataModel);
-            model.Condition = "id = @id_user@ and email = @email@ and deleted = 0 and active = 1";
-            json = Flow(ref model);
+            string messageLog = "";
+
+            json = ZionDto.MapperDto(ref dto, requestData);
 
             if (GetJson("status") != "success")
             {
-                return json;
+                messageLog = ZionDto.GetResult();
+                return ZionResponse.Fail(23, "", 400, "auto", messageLog);
             }
 
+            sqlQuery = "select * from auth_mf where id_user = " + SqlPar(dto.Id_user.ToString()) + " and id_partner = (select id from partner where token = " + SqlPar(dto.Token_partner) + ") and active = 1";
+            json = Get();
+
+            if (GetJson("status") != "success" || ZionConv.ToInt(GetJsonSummary("records_json")) == 0)
+            {
+                throw new ZionException(59, "auto", "ValidateCode()", "Partner not found - Unable to authenticate the API with the data received in Dto: " + LastSqlSentence);
+            }
+
+            string id_partner = GetJsonRecord(1, "id_partner");
+            string id_user = GetJsonRecord(1, "id_user");
+            string public_key = GetJsonRecord(1, "public_key");
+            string private_key = GetJsonRecord(1, "private_key");
+            string hashKey = id_user + id_partner + public_key;
+            ZionEncrypter hash = new ZionEncrypter();
+
+            if (!hash.CompareHash(private_key, hashKey))
+            {
+                messageLog = $"user: {id_user} partner: {id_partner} - Suspected violation of the public key {public_key} in the generation of totp. Private key result incorrect";
+                ZionAlert.Save(messageLog);
+                return ZionResponse.Fail(63, "", 400, "auto", messageLog);
+            }
+                                      
             // Code Validation
             ZionTotp totp = new ZionTotp();
-            json = totp.VerifyTotp(dto.Code, ZionSecurity.UnMask(model.RecordNew.token_auth_mf, 64));
+            json = totp.VerifyTotp(dto.Code, ZionSecurity.UnMask(public_key, 64));
             int success = GetJson("status") == "success" ? 1 : 0;
             int userLogCode = success == 1 ? 23 : 24;
 
